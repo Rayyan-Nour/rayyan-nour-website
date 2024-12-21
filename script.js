6326951eeaab12dda5b9b7b4796721dac85d7d29
Rayyan Nour <112745587+Rayyan-Nour@users.noreply.github.com>
2024-12-21 01:37:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Optional JavaScript for smooth scrolling or other interactivity
-// Smooth scrolling for same-page links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
